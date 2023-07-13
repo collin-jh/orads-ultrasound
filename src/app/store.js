8929f90b311dcab0ab949b175d5e7fb27dccff5e
Collin Henderson <cjhend00@gmail.com>
2023-07-12 19:13:15 -0500

    cleaning for new project, orads project moved to orads-mri(local only)

--- src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import calculatorQuestionsReducer from '../features/calculatorQuestionsSlice';
-
-export default configureStore({
-    reducer: {
-        calculatorQuestions: calculatorQuestionsReducer,
-    }
-});
